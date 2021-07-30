a414a99e96d0cc1c117dc0e9b421ebb455819647
Angelica <Angelica137@users.noreply.github.com>
2021-07-31 00:10:35 +0100

    daysBetweenDates returns 583, green

--- scripts/daysOld.py
+++ scripts/daysOld.py
@@ -18,6 +18,7 @@ def daysBetweenDates(y1, m1, d1, y2, m2, d2):
         if m1 == m2:
             days = d2 - d1
     elif(y2 - y1 == 1):
+            
         i = 11
         y1_days = 0
         y2_days = 0
@@ -35,6 +36,11 @@ def daysBetweenDates(y1, m1, d1, y2, m2, d2):
         days_month1 = daysOfMonths[m1 -1] - d1
 
         days = y1_days + y2_days + days_month1 + d2
+
+        if isLeapYear(y1) == 366 and m1 < 3:
+            days += 1
+        if isLeapYear(y2) == 366 and m2 > 1:
+            days += 1
 
         #islice(daysOfMonths, m1, len(daysOfMonths)-1)
 
