fa0c6697ea1a08209609f8eca28fa2f5cc400ea0
Angelica <Angelica137@users.noreply.github.com>
2021-07-30 22:04:32 +0100

    isLeapYear returns 365 is year is not divisible by 4, green

--- scripts/daysOld.py
+++ scripts/daysOld.py
@@ -4,6 +4,8 @@ daysOfMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 def isLeapYear(year):
     if year % 4 == 0:
         return 366
+    else:
+        return 365
 
 
 def daysBetweenDates(y1, m1, d1, y2, m2, d2):
--- tests/test_daysOld.py
+++ tests/test_daysOld.py
@@ -6,4 +6,4 @@ def test_isLeapYear_returns_366_if_year_divisible_by_4():
 
 
 def test_isLeapYear_returns_364_if_year_not_divisible_by_4():
-    assert isLeapYear(2004) == 365
+    assert isLeapYear(2003) == 365
