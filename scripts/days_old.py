3bddf64ee73a69c2221d3d24e17afd6ea5324b84
Angelica <Angelica137@users.noreply.github.com>
2021-07-30 21:44:25 +0100

    add skeleton code

---
+++ scripts/days_old.py
@@ -0,0 +1,9 @@
+daysOfMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+
+def isLeapYear(year):
+    '''somehtign happens here'''
+
+
+def daysBetweenDates(y1, m1, d1, y2, m2, d2):
+    '''somethign else happns here'''
