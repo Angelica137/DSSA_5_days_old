83eb5a57e3c74f0bd206371a7a693e07d7457995
Angelica <Angelica137@users.noreply.github.com>
2021-07-30 22:24:31 +0100

    isLeapYear returns 366 if year is divisible by 4, 100 and 400, green

--- scripts/daysOld.py
+++ scripts/daysOld.py
@@ -2,10 +2,14 @@ daysOfMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 
 def isLeapYear(year):
-    if year % 4 == 0 and year % 100 != 0:
-        return 366
-    else:
-        return 365
+    days = 365
+    if year % 4 == 0:
+        days = 366
+        if year % 100 == 0:
+            days = 365
+            if year % 400 == 0:
+                days = 366
+    return days
 
 
 def daysBetweenDates(y1, m1, d1, y2, m2, d2):
